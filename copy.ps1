e9e8d96349b2e619d4e1053d5c771a9de8686309
VimHater <24520826@gm.uit.edu.vn>
2026-06-14 21:16:50 +0700

    update

--- copy.ps1
+++ copy.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 Set-PSDebug -Trace 1
 
 New-Item -ItemType Directory -Force -Path "$env:LOCALAPPDATA/neovide" | Out-Null
-Copy-Item -Recurse -Force ./neovide/* "$env:LOCALAPPDATA/neovide/"
+Copy-Item -Recurse -Force ./neovide/* "$env:USERPROFILE/AppData/Roaming/neovide/"
 Copy-Item -Force ./wezterm.lua "$env:USERPROFILE/.wezterm.lua"
